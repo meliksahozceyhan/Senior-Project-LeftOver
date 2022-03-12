dc59f553c646cbbef7be2573427abfdc6671aa13
furkankilic11 <99671045+furkankilic11@users.noreply.github.com>
2022-03-12 14:55:43 +0300

    feat: getItems and searchItems endpoints.

--- server/src/model/item/item.controller.ts
+++ server/src/model/item/item.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post, Request, Query } from '@nestjs/common'
 import { Crud, CrudController } from '@nestjsx/crud'
+import { SkipAuth } from 'src/decorators/decorators'
 import { Item } from './entity/item.entity'
 import { ItemService } from './item.service'
 
@@ -15,4 +16,14 @@ import { ItemService } from './item.service'
 @Controller('/item')
 export class ItemController implements CrudController<Item> {
 	constructor(public service: ItemService) {}
+
+	@Get('/getItems')
+	public async getItems(@Request() req): Promise<any> {
+		return this.service.getItems()
+	}
+
+    @Get('/searchItems')
+	public async searchItems(@Request() req, @Query('searchValue') searchValue: string): Promise<any> {
+		return this.service.searchItems(searchValue);
+	}
 }
